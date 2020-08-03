7f1df21fab293cd5a8c700efd40751912cbb0ad8
IvanPyatyrov <36262990+IvanPyatyrov@users.noreply.github.com>
2020-08-03 18:31:01 +1000

    Updates

---
+++ precache-manifest.2d6889b808ca7fb2a01bb07be0a27747.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b31a79f8a546174b285adb90287828e6",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "17b22ebcddab24f1a126",
+    "url": "/deploy-react/static/css/3.93589f11.chunk.css"
+  },
+  {
+    "revision": "39dcda765eb750295a2b",
+    "url": "/deploy-react/static/css/4.71995090.chunk.css"
+  },
+  {
+    "revision": "bef41ac27c0df5692f5b",
+    "url": "/deploy-react/static/css/main.7a05bc0c.chunk.css"
+  },
+  {
+    "revision": "4cf053df5597d1708d79",
+    "url": "/deploy-react/static/js/2.de83630e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/deploy-react/static/js/2.de83630e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17b22ebcddab24f1a126",
+    "url": "/deploy-react/static/js/3.26af13bd.chunk.js"
+  },
+  {
+    "revision": "39dcda765eb750295a2b",
+    "url": "/deploy-react/static/js/4.257209d3.chunk.js"
+  },
+  {
+    "revision": "bef41ac27c0df5692f5b",
+    "url": "/deploy-react/static/js/main.75ca8b69.chunk.js"
+  },
+  {
+    "revision": "7977e821a4555c523193",
+    "url": "/deploy-react/static/js/runtime-main.0513f94f.js"
+  },
+  {
+    "revision": "f4d7875d836558dc64082fcc6d7d0b6c",
+    "url": "/deploy-react/static/media/Preloader.f4d7875d.svg"
+  },
+  {
+    "revision": "385e14270581eab76648f2e992709713",
+    "url": "/deploy-react/static/media/user.385e1427.png"
+  }
+]);
